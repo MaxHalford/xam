c147cd795e8e6d98b5fecea01fd0f70d9f1dd5cb
Max Halford <maxhalford25@gmail.com>
2017-02-04 12:40:26 +0100

    Add setup.py and LICENSE files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    author='Max Halford',
+    author_email='maxhalford25@gmail.com',
+    description='Data science utilities library',
+    license='MIT',
+    name='xam',
+    packages=find_packages(exclude=['tests']),
+    url='https://github.com/MaxHalford/xam',
+    version='0.0.1dev',
+)
