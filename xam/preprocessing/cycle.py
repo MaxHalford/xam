5fe2e26e960a16cc5743ac2945bbf96da0ba8b15
Max Halford <maxhalford25@gmail.com>
2017-04-21 12:37:11 +0200

    CycleTransformer can now be used in a pipeline

--- xam/preprocessing/cycle.py
+++ xam/preprocessing/cycle.py
@@ -9,17 +9,16 @@ from sklearn.base import TransformerMixin
 class CycleTransformer(BaseEstimator, TransformerMixin):
 
     def fit(self, X, y=None, **fit_params):
+        self.maximums_ = np.max(X, axis=0)
         return self
 
     @staticmethod
-    def cosine_sine_transform(x):
-        maximum = np.max(x)
+    def cosine_sine_transform(x, maximum):
         radians = 2 * np.pi * x
         return np.cos(radians / (maximum + 1)) + np.sin(radians / (maximum + 1))
 
     def transform(self, X, **transform_params):
-        return np.apply_along_axis(
-            func1d=lambda x: self.cosine_sine_transform(x),
-            axis=0,
-            arr=X
-        )
+        return np.array([
+            self.cosine_sine_transform(x, maximum)
+            for x, maximum in zip(X.T, self.maximums_)
+        ]).T
