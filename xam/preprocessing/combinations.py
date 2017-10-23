1b23977af2f390d2e1d655383a57610265c6f369
Max Halford <maxhalford25@gmail.com>
2017-10-23 11:58:53 +0200

    Store new col names in feature combinations

--- xam/preprocessing/combinations.py
+++ xam/preprocessing/combinations.py
@@ -29,6 +29,8 @@ class FeatureCombiner(BaseEstimator, TransformerMixin):
         if self.columns is None:
             self.columns = [col for col in X.columns if dtypes[col] in ('object', 'category')]
 
+        self.new_column_names_ = []
+
         return self
 
     def transform(self, X, y=None):
@@ -39,6 +41,7 @@ class FeatureCombiner(BaseEstimator, TransformerMixin):
         for order in self.orders:
             for combo in itertools.combinations(self.columns, order):
                 col_name = self.separator.join(combo)
+                self.new_column_names_.append(col_name)
                 X[col_name] = X[combo[0]].apply(str).str.cat([
                     X[col].apply(str)
                     for col in combo[1:]
