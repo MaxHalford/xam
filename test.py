009cabdeadac2b0f20b4a8dc229d2e736bcc1f69
MaxHalford <maxhalford25@gmail.com>
2018-02-06 15:48:26 +0100

    Add EWM optimisation

--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from sklearn import metrics
-import xam
-
-series = pd.Series([1, 2, 3, 4, 3, 4, 5, 4, 5, 6])
-ewm = xam.tsa.calc_optimized_ewm(series, metric=metrics.mean_squared_error, seed=42)
-print(ewm)
